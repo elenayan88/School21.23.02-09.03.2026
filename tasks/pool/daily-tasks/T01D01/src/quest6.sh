23acf3dc6d5e8525c4d16b0fb3ea8f50cc8dd748
Elena Yan <elenayan1488@gmail.com>
2026-02-04 19:47:48 +0500

    Leave only initial tasks

--- tasks/pool/daily-tasks/T01D01/src/quest6.sh
+++
@@ -1,7 +0,0 @@
-cd ai_help
-chmod +x *
-./keygen.sh
-cd key
-rm file*
-cd ..
-./unifier.sh
--- tasks/pool/daily-tasks/T03D03/src/named_hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int name;
-    scanf("%d", &name);
-    printf("Hello, %d!", name);
-    return 0;
-}
