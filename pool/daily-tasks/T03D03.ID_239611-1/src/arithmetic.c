8a2d3cf3ea496a0a3c2fe7e6c8ba9f0f6772e8dc
Elena Yan <elenayan1488@gmail.com>
2026-02-08 21:18:44 +0500

    Quest3

--- pool/daily-tasks/T03D03.ID_239611-1/src/arithmetic.c
+++ pool/daily-tasks/T03D03.ID_239611-1/src/arithmetic.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-int sum(int a, int b);
+int main() {
+    int a, b;
 
-int main()
-{
+    if (scanf("%d %d", &a, &b) != 2) {
+        printf("n/a\n");
+        return 1;
+    }
+
+    printf("%d %d %d ", a + b, a - b, a * b);
+
+    if (b == 0) {
+        printf("n/a\n");
+    } else {
+        printf("%d\n", a / b);
+    }
+
     return 0;
-}
-
-int sum(int a, int b)
-{
-    return (2 * a * b) / (a - b);
 }
