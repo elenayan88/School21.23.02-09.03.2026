9efad28ac3af7a5d4b45538979164d43e94459e9
Elena Yan <elenayan1488@gmail.com>
2026-02-11 19:44:53 +0500

    Quest1

--- pool/daily-tasks/T05D08.ID_239624-1/src/maxmin.c
+++ pool/daily-tasks/T05D08.ID_239624-1/src/maxmin.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 
-void maxmin(int prob1, int prob2, int prob3, int *max, int min);
+void maxmin(int prob1, int prob2, int prob3, int* max, int* min);
 
-/* Find a max & min probabilities */
-int main()
-{
+int main() {
     int x, y, z;
-    scanf("%d %d", x, y, z);
+    char term;
+
+    if (scanf("%d %d %d%c", &x, &y, &z, &term) < 3 || (term != '\n' && term != ' ' && term != '\0')) {
+        printf("n/a\n");
+        return 0;
+    }
 
     int max, min;
 
-    maxmin(x, y, z, max, min);
+    maxmin(x, y, z, &max, &min);
 
-    printf("%d %d", max, min);
+    printf("%d %d\n", max, min);
 
     return 0;
 }
 
-/* This function should be kept !!! (Your AI) */
-/* But errors & bugs should be fixed         */
-void maxmin(int prob1, int prob2, int prob3, int *max, int min)
-{
-    *max = min = prob1;
+void maxmin(int prob1, int prob2, int prob3, int* max, int* min) {
+    *max = *min = prob1;
 
-    if (prob2 > max)
-        max = prob2;
-    if (prob2 < min)
-        min = prob2;
+    if (prob2 > *max) *max = prob2;
+    if (prob2 < *min) *min = prob2;
 
-    if (prob3 > max)
-        max = prob3;
-    if (prob3 < min)
-        min = prob3;
+    if (prob3 > *max) *max = prob3;
+    if (prob3 < *min) *min = prob3;
 }
