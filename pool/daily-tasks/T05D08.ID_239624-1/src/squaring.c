f7ebb25b34fab325d6b81e476d823118eb947c5b
Elena Yan <elenayan1488@gmail.com>
2026-02-11 22:43:50 +0500

    Quest2

--- pool/daily-tasks/T05D08.ID_239624-1/src/squaring.c
+++ pool/daily-tasks/T05D08.ID_239624-1/src/squaring.c
@@ -1,35 +1,54 @@
 #include <stdio.h>
 #define NMAX 10
 
-int input(int *a, int *n);
-void output(int *a, int n);
-void squaring(int *a, int n);
+int input(int* data, int* n);
+void output(int* data, int n);
+void squaring(int* data, int n);
 
-int main()
-{
+int main() {
     int n, data[NMAX];
-    input(data, n);
+
+    if (input(data, &n) == 0) {
+        printf("n/a\n");
+        return 0;
+    }
+
     squaring(data, n);
     output(data, n);
 
     return 0;
 }
 
-int input(int *a, int *n)
-{
-    scanf("%d", n);
-    for (int *p = a; p - a < *n; p++)
-    {
-        scanf("%d", p);
+int input(int* data, int* n) {
+    if (scanf("%d", n) != 1 || *n <= 0 || *n > NMAX) {
+        return 0;
+    }
+
+    for (int* p = data; p - data < *n; p++) {
+        if (scanf("%d", p) != 1) {
+            return 0;
+        }
+    }
+
+    char c;
+    if (scanf("%c", &c) == 1 && c != '\n' && c != ' ' && c != '\0') {
+    }
+
+    return 1;
+}
+
+void squaring(int* data, int n) {
+    for (int* p = data; p - data < n; p++) {
+        *p = (*p) * (*p);
     }
 }
 
-void output(int *a, int n)
-{
-    // NOTHING
-}
+void output(int* data, int n) {
+    for (int* p = data; p - data < n; p++) {
+        printf("%d", *p);
 
-void squaring(int *a, int n)
-{
-    // NOTHING
+        if (p - data < n - 1) {
+            printf(" ");
+        }
+    }
 }
