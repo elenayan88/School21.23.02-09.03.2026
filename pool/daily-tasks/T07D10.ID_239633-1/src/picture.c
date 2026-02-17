44a973cc7c31d33e9986a2cb03f27876a51293e5
Elena Yan <elenayan1488@gmail.com>
2026-02-17 20:01:15 +0500

    Quest5

--- pool/daily-tasks/T07D10.ID_239633-1/src/picture.c
+++ pool/daily-tasks/T07D10.ID_239633-1/src/picture.c
@@ -1,57 +1,85 @@
+#include <stdio.h>
+
 #define N 15
 #define M 13
 
-void transform(int *buf, int **matr, int n, int m);
-void make_picture(int **picture, int n, int m);
-void reset_picture(int **picture, int n, int m);
+void transform(int* buf, int** matr, int n, int m);
+void make_picture(int** picture, int n, int m);
+void reset_picture(int** picture, int n, int m);
+void output(int** picture, int n, int m);
 
-void main()
-{
-   int picture_data[N][M];
-   int *picture[N];
-   transform(picture_data, picture, N, M);
+int main() {
+    int picture_data[N][M];
+    int* picture[N];
+    transform(&picture_data[0][0], picture, N, M);
 
-   make_picture(picture, );
+    make_picture(picture, N, M);
+    output(picture, N, M);
+
+    return 0;
 }
 
-void make_picture(int **picture, int n, int m)
-{
-   int frame_w[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-   int frame_h[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-   int tree_trunk[] = {7, 7, 7, 7};
-   int tree_foliage[] = {3, 3, 3, 3};
-   int sun_data[6][5] = {{0, 6, 6, 6, 6},
-                         {0, 0, 6, 6, 6},
-                         {0, 0, 6, 6, 6},
-                         {0, 6, 0, 0, 6},
-                         {0, 0, 0, 0, 0},
-                         {0, 0, 0, 0, 0}};
+void make_picture(int** picture, int n, int m) {
+    int frame_w[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int frame_h[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int tree_trunk[] = {7, 7, 7, 7};
+    int tree_foliage[] = {3, 3, 3, 3};
+    int sun_data[6][5] = {{0, 6, 6, 6, 6}, {0, 0, 6, 6, 6}, {0, 0, 6, 6, 6},
+                          {0, 6, 0, 0, 6}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
 
-   reset_picture(picture, n, m);
+    reset_picture(picture, n, m);
 
-   int length_frame_w = sizeof(frame_w) / sizeof(frame_w[0]);
+    for (int i = 0; i < m; i++) {
+        picture[0][i] = frame_w[i];
+        picture[n - 1][i] = frame_w[i];
+        picture[n / 2][i] = frame_w[i];
+    }
 
-   for (int i = 0; i < length_frame_w; i++)
-   {
-      picture[0][i] = frame_w[i];
-   }
+    for (int i = 0; i < n; i++) {
+        picture[i][0] = frame_h[i];
+        picture[i][m - 1] = frame_h[i];
+        picture[i][m / 2] = frame_h[i];
+    }
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 5; j++) {
+            picture[1 + i][7 + j] = sun_data[i][j];
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        picture[10 + i][2] = tree_trunk[i];
+        picture[10 + i][3] = tree_trunk[i];
+        picture[10 + i][4] = tree_trunk[i];
+    }
+    picture[10][2] = tree_trunk[0];
+
+    for (int i = 0; i < 4; i++) {
+        picture[6][2 + i] = tree_foliage[i];
+        picture[7][2 + i] = tree_foliage[i];
+        picture[8][2 + i] = tree_foliage[i];
+        picture[9][2 + i] = tree_foliage[i];
+    }
 }
 
-void reset_picture(int **picture, int n, int m)
-{
-   for (int i = 0; i < m; i++)
-   {
-      for (int j = 0; j < n; j++)
-      {
-         picture[i][j] = 0;
-      }
-   }
+void reset_picture(int** picture, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            picture[i][j] = 0;
+        }
+    }
 }
 
-void transform(int *buf, int **matr, int n, int m)
-{
-   for (int i = 0; i < n; i++)
-   {
-      matr[i] = buf + i * m;
-   }
+void transform(int* buf, int** matr, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        matr[i] = buf + i * m;
+    }
+}
+
+void output(int** picture, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d%s", picture[i][j], (j < m - 1) ? " " : "");
+        }
+        if (i < n - 1) printf("\n");
+    }
 }
